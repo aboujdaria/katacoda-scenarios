340e9ef06e28926a04443310864597bfe09277e1
Ali BOUJDARIA <boujali@yahoo.fr>
2020-04-27 14:23:29 +0200

    fix encoding

--- openstackdgfip/environnement.sh
+++ openstackdgfip/environnement.sh
@@ -1,8 +1,8 @@
 pip3 install python-openstackclient
 pip3 install python-heatclient
-mkdir Exercices_corrigés/
+mkdir Exercices_correction/
 mv exercice_*_correction.yaml Exercices_corrigés/
-mkdir Exercices_non_corrigés/
-mv exercice_*.yaml Exercices_non_corrigés/
+mkdir Exercices/
+mv exercice_*.yaml Exercices/
 clear
 echo "L'environnement est prêt."
