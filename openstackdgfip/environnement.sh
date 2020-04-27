9426dba2e5b00716a39adbe397b760cd73de3cf1
Ali BOUJDARIA <boujali@yahoo.fr>
2020-04-27 14:09:29 +0200

    push assests

--- openstackdgfip/environnement.sh
+++ openstackdgfip/environnement.sh
@@ -1,4 +1,8 @@
 pip3 install python-openstackclient
 pip3 install python-heatclient
+mkdir Exercices_corrigés/
+mv exercice_*_corrigés.yaml Exercices_corrigés/
+mkdir Exercices_non_corrigés/
+mv exercice_*.yaml Exercices_non_corrigés/
 clear
 echo "L'environnement est prêt."
